fd0d82b9f16ddabb4dd70211851e569fce61fcef
Malisha Maharjan <malishamaharjan00@gmail.com>
2022-12-24 20:16:18 +0545

    Removed unnecessary environment variables

--- Login/urls.py
+++ Login/urls.py
@@ -13,6 +13,6 @@ environ.Env.read_env()
 urlpatterns = [
   # path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  path(env('STUDENT_REGISTER'), views.studentRegister, name="register"),
+  path('register', views.studentRegister, name="register"),
   path('getInfo', views.getInfo, name="Info"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
