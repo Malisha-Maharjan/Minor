86a95bff5274743dff3b878ec00c1783bb813d3a
Malisha Maharjan <malishamaharjan00@gmail.com>
2023-01-23 12:21:55 +0545

    Refactoring

--- Account/urls.py
+++ Account/urls.py
@@ -16,15 +16,17 @@ urlpatterns = [
   # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
   path('user/create', views.userCreate, name="register"),
   path('getInfo', views.getAllInfo, name="Info_all"),
-  path('getInfo/student/<int:id>', views.getInfo, name="Info"),
-  path('delete/student/<int:id>', views.deleteInfo, name="delete"),
-  path('update/student/<int:id>', views.updateInfo, name="update"),
+  path('getInfo/<str:username>', views.getInfo, name="Info"),
+  path('delete/<str:username>', views.deleteInfo, name="delete"),
+  path('update/<str:username>', views.updateInfo, name="update"),
   path('login', views.login),
   path('hello', views.hello),
-  path('student/<int:id>', views.student),
+  path('student/<str:username>', views.student),
   path('manualPayment/<str:username>', paymentviews.manualPayment),
   path('details/<str:username>', views.details),
   path('due/<str:username>', paymentviews.due),
-  path('khalti', paymentviews.khaltiVerify)
+  path('khalti', paymentviews.khaltiVerify),
+  # path('paymentDetails/<str:username>', paymentviews.paymentDetails),
+  path('bill/<str:username>', paymentviews.bill)
   
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
